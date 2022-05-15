c25f2109cc81e4ccb24919324dd1d5d303a90083
Paitson <839098328@qq.com>
2022-05-15 10:54:45 +0800

    DXDS
    DXDS

---
+++ dxds.justlists.ts
@@ -0,0 +1,21 @@
+{
+    "name":"DXDS Default List",
+    "tokens":[
+        {
+            "symbol":"DXDS",
+            "address":"TEqK3awVu3jy6xEg6LpNYwUgTHdzPBmzGz",
+            "chainId":1,
+            "decimals":18,
+            "name":"DXDS TOKEN",
+            "logoURI":"https://coin.top/production/upload/logo/TMgEtvLeWUnzYibGJJt3YxQswEsQNYpqz7.jpeg?t=1646489685590"
+        },
+        
+    ],
+    "logoURI":"https://coin.top/production/upload/logo/TMgEtvLeWUnzYibGJJt3YxQswEsQNYpqz7.jpeg?t=1646489685590",
+    "version":{
+        "patch":1,
+        "major":1,
+        "minor":0
+    },
+    "timestamp":1652583227000
+}
